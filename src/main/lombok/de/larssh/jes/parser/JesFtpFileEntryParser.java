ee54ed50716c2275dd9309eb68d7f132cf26cf39
Lars Knickrehm <mail@lars-sh.de>
2019-03-18 22:34:29 +0100

    Remove unused import

--- src/main/lombok/de/larssh/jes/parser/JesFtpFileEntryParser.java
+++ src/main/lombok/de/larssh/jes/parser/JesFtpFileEntryParser.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.regex.Matcher;
